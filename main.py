7d1dd7ce34704dc122d7ccbc536326ffabd80386
shweta9007 <132440855+shweta9007@users.noreply.github.com>
2024-05-12 07:50:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -110,9 +110,9 @@ def loopthread(message,otherss=False):
 # start command
 @app.on_message(filters.command(["start"]))
 def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
-    app.send_message(message.chat.id, f"__👋 Hi **{message.from_user.mention}**,  i Am URL-BYPASS GROUP BOT, Just Send Me Any Supported Links And I Will YOU get You Results & One More SomeTime I Don't Give You Results It Doesn't Mean I'M Angry With You I Am Also An Online Machine, I Also Get Tired I hope you are understand Thank you🤝❤️.\nCheckout /help to Read More__",
+    app.send_message(message.chat.id, f"__👋 Hi **{message.from_user.mention}**,  i Am URL-BYPASS GROUP BOT, Just Send Me Any Supported Links And I Will YOU get You Results & One More SomeTime I Don't Give You Results It Doesn't Mean I'M Angry With You I Am Also An Online Machine, I Also Get Tired that's why I need Rest I hope you are understand Thank you🤝❤️.\nCheckout /help to Read More__",
     reply_markup=InlineKeyboardMarkup([
-        [ InlineKeyboardButton("🌐 Source Code", url="https://github.com/bipinkrish/Link-Bypasser-Bot")],
+        [ InlineKeyboardButton("⚡Authorised Group", url="https://t.me/Bypassgroup")],
         [ InlineKeyboardButton("Replit", url="https://replit.com/@bipinkrish/Link-Bypasser#app.py") ]]), 
         reply_to_message_id=message.id)
 
